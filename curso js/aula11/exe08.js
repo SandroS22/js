48692e3d5310e4d238d61d8dc82da87e644a7f02
Sandro Santana <62521881+SandroS22@users.noreply.github.com>
2023-01-20 16:34:03 -0300

    Update exe08.js

--- curso js/aula11/exe08.js
+++ curso js/aula11/exe08.js
@@ -3,5 +3,5 @@ console.log(`A velocidade do seu carro é ${vel}`)
 if (vel > 60){
     console.log('Você ultrapassou a velocidade permitida. MULTADO!')
 } else{
-    console.log('Bostinha!!!')
+    console.log('Use sempre cinto de segurança!!!')
 }
